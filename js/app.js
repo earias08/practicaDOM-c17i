01a9f5bdb03e778bdd399f9f9f83e98d2351513c
Emilse <emilsearias@gmail.com>
2021-09-23 18:49:38 -0300

    agregamos otra forma de insertar html desde nuestro archivo de javascript

--- js/app.js
+++ js/app.js
@@ -23,21 +23,30 @@ function verMas(){
 
     if(boton.innerHTML == 'Ver mas...'){
             // paso 2 - crear el elemento <p>
-            let parrafo = document.createElement('p'); //<p></p>
-            // pase 3 - agregar datos al elemento creado
-            parrafo.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, dolorum omnis. Asperiores dolor nam, blanditiis cum atque assumenda! Quas rerum harum fugit ullam cumque voluptatem quaerat nam quia doloremque non eligendi facilis voluptatum, corporis aut. Repellendus modi saepe minus quia unde aliquam sapiente accusamus aperiam suscipit amet, molestiae numquam explicabo?';
-            parrafo.className = 'lead';
-            console.log(articulos[1].children[1]);
-            // paso 4 - unir el elemento padre con el nodo hijo
-            // articulos[1].appendChild(parrafo); //agrega nodos hijos al final
-            articulos[1].insertBefore(parrafo, articulos[1].children[2]);
-           
+            // Forma 1
+            // let parrafo = document.createElement('p'); //<p></p>
+            // // pase 3 - agregar datos al elemento creado
+            // parrafo.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, dolorum omnis. Asperiores dolor nam, blanditiis cum atque assumenda! Quas rerum harum fugit ullam cumque voluptatem quaerat nam quia doloremque non eligendi facilis voluptatum, corporis aut. Repellendus modi saepe minus quia unde aliquam sapiente accusamus aperiam suscipit amet, molestiae numquam explicabo?';
+            // parrafo.className = 'lead';
+            // console.log(articulos[1].children[1]);
+            // // paso 4 - unir el elemento padre con el nodo hijo
+            // // articulos[1].appendChild(parrafo); //agrega nodos hijos al final
+            // articulos[1].insertBefore(parrafo, articulos[1].children[2]);
+
+            // Forma 2
+            articulos[1].innerHTML += '<p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores tempora rem velit minima fugiat in libero cupiditate totam eaque ipsam nulla qui consequatur praesentium aliquid impedit commodi corrupti, quaerat consectetur quia omnis soluta numquam. Magnam explicabo, odit laudantium nulla eos dolorem dolores eius earum sed quis vel aliquam totam mollitia!</p>';
+
             boton.innerHTML = 'Ocultar';
             boton.className = 'btn btn-outline-danger';
     }else{
         console.log('aqui quiero eliminar el elemento hijo');
-    }
-
-
-  
+        console.log(articulos[1].children[2]);
+        console.log(articulos[1].hasChildNodes());
+        if(articulos[1].hasChildNodes() && articulos[1].children.length > 2){
+            // borrar un nodo hijo
+            articulos[1].removeChild(articulos[1].children[2]);
+            boton.innerHTML = 'Ver mas...';
+            boton.className = 'btn btn-outline-primary';
+        }
+    }  
 }
